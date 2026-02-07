ad695e6106af2cfda7d5facd44feee63a72dd239
swamyhr <swamyrangannanayaka@gmail.com>
2026-02-07 22:04:33 +0530

    updated inventoryservice class

--- inventory-service/src/main/java/com/ordersystem/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/ordersystem/inventoryservice/service/InventoryService.java
@@ -1,5 +1,36 @@
 package com.ordersystem.inventoryservice.service;
 
+import com.ordersystem.inventoryservice.model.InventoryItem;
+import com.ordersystem.inventoryservice.repository.InventoryRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class InventoryService {
 
+  private InventoryRespository inventoryRespository;
+
+  @Autowired
+  public InventoryService(InventoryRespository inventoryRespository) {
+    this.inventoryRespository = inventoryRespository;
+  }
+
+
+  public List<InventoryItem> getAllInventoryItems(){
+    return inventoryRespository.findAll();
+  }
+
+  public InventoryItem getItemById(String id) {
+    return inventoryRespository.findById(id).orElse(null);
+  }
+
+  public InventoryItem createItem(InventoryItem item) {
+    return inventoryRespository.save(item);
+  }
+
+  public void deleteItem(String id) {
+    inventoryRespository.deleteById(id);
+  }
 }
