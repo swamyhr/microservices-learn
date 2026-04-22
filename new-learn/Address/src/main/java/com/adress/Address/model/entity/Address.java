ddebfc2af020d33815120965f257baaef5b28f82
swamyhr <swamyrangannanayaka@gmail.com>
2026-04-22 22:04:13 +0530

    updated Address Enum and Address Entity

--- new-learn/Address/src/main/java/com/adress/Address/model/entity/Address.java
+++ new-learn/Address/src/main/java/com/adress/Address/model/entity/Address.java
@@ -1,9 +1,7 @@
 package com.adress.Address.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.adress.Address.model.enums.AddressType;
+import jakarta.persistence.*;
 
 @Entity
 public class Address {
@@ -19,5 +17,91 @@ public class Address {
 
   private String city;
 
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getEmpId() {
+    return empId;
+  }
+
+  public void setEmpId(Long empId) {
+    this.empId = empId;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  public void setStreet(String street) {
+    this.street = street;
+  }
+
+  public Long getPinCode() {
+    return pinCode;
+  }
+
+  public void setPinCode(Long pinCode) {
+    this.pinCode = pinCode;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public AddressType getAddressType() {
+    return addressType;
+  }
+
+  public void setAddressType(AddressType addressType) {
+    this.addressType = addressType;
+  }
+
   private String country;
+
+  @Enumerated(EnumType.STRING)
+  private AddressType addressType;
+
+  public Address() {
+
+  }
+
+  public Address(Long id, Long empId, String street, Long pinCode, String city, String country, AddressType addressType) {
+    this.id = id;
+    this.empId = empId;
+    this.street = street;
+    this.pinCode = pinCode;
+    this.city = city;
+    this.country = country;
+    this.addressType = addressType;
+  }
+
+  @Override
+  public String toString() {
+    return "Address{" +
+        "id=" + id +
+        ", empId=" + empId +
+        ", street='" + street + '\'' +
+        ", pinCode=" + pinCode +
+        ", city='" + city + '\'' +
+        ", country='" + country + '\'' +
+        ", addressType=" + addressType +
+        '}';
+  }
 }
---
+++ new-learn/Address/src/main/java/com/adress/Address/model/enums/AddressType.java
@@ -0,0 +1,7 @@
+package com.adress.Address.model.enums;
+
+public enum AddressType {
+
+  PERMANENT,
+  TEMPORARY
+}
