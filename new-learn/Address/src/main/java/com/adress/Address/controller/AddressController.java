5dc62de9bc4b80f1a010b38b6e286314d32b1098
swamyhr <swamyrangannanayaka@gmail.com>
2026-05-04 20:05:43 +0530

    Added updateAddress controller code in service

--- new-learn/Address/src/main/java/com/adress/Address/controller/AddressController.java
+++ new-learn/Address/src/main/java/com/adress/Address/controller/AddressController.java
@@ -5,10 +5,7 @@ import com.adress.Address.model.dto.AddressRequest;
 import com.adress.Address.service.AddressService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +23,13 @@ public class AddressController {
   public ResponseEntity<List<AddressDTO>> saveAddresses(@RequestBody AddressRequest addressRequest){
     List<AddressDTO> savedListAddress = addressService.saveAddress(addressRequest);
     return new ResponseEntity<>(savedListAddress, HttpStatus.CREATED);
+  }
+
+  @PutMapping("/update/{empId}")
+  public ResponseEntity<List<AddressDTO>> updateAddress(
+      @RequestBody AddressRequest addressDTO, @PathVariable Long empId) {
+    List<AddressDTO> response = addressService.updateAddress(addressDTO);
+    return new ResponseEntity<>(response, HttpStatus.OK);
   }
 
 }
