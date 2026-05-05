eac1ade73db2fad0c568c05db3e5c6cbe9439401
swamyhr <swamyrangannanayaka@gmail.com>
2026-05-05 20:33:46 +0530

    service discovery app added

---
+++ new-learn/Address/src/test/java/com/adress/Address/AddressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adress.Address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
